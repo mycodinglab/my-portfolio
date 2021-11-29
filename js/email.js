9f4ce025639842be5948694cd6f0a6849fe241a6
mycodinglab <almamun2b@gmail.com>
2021-11-29 23:10:30 +0600

    Contact Form Complete

--- js/email.js
+++ js/email.js
@@ -2,13 +2,14 @@ function getElement(elementId) {
     return document.getElementById(elementId);
 }
 
+
 const sendMail = (params) => {
     var tempParams = {
-        from_name: getElement("name").value,
-        from_email: getElement("email").value,
-        message: getElement("message").value
+        from_name: getElement("fromName").value,
+        to_name: getElement("toName").value,
+        message: getElement("msg").value
     };
-    emailjs.send('service_6ybrv6v', 'template_byxxujd', tempParams)
+    emailjs.send('service_6ybrv6v', 'template_p1b40xd', tempParams)
         .then(
             function (res) {
                 console.log('SUCCESS!', res.status);
